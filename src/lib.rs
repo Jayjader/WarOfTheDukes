b7bd8ace5fcdb5a221273b2fd1b6e3e53b729e9d
Noah Jay <njay47@gmail.com>
2019-11-24 00:23:22 +0100

    Finish converting from Hello World; Start Board implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-use gdnative::*;
+#[macro_use]
+extern crate gdnative as godot;
+use hex2d;
+use std::collections::HashMap;
 
 /// The "Unit" enum. Represents the 4 unit types of the game:
 ///  Infantry, Cavalry, Artillery, Duke
-enum Unit {
+pub enum Unit {
     Duke,
     Infantry,
     Cavalry,
@@ -40,6 +43,52 @@ impl Defense for Unit {
     }
 }
 
+/// The Game Board. Is a 2D grid of hexagonal Tiles.
+#[derive(NativeClass)]
+#[inherit(godot::Node)]
+pub struct Board {
+    tile_size: hex2d::Spacing,
+    tiles: HashMap<(u32, u32), Tile>,
+}
+
+pub const TILE_SIZE: f32 = 10 as f32;
+
+#[methods]
+impl Board {
+    fn new(tile_size: hex2d::Spacing) -> Board {
+        Board {
+            tile_size,
+            tiles: HashMap::new(),
+        }
+    }
+    // Godot's init hook
+    fn _init(_owner: godot::Node) -> Self {
+        use Tile::*;
+        let mut board = Board::new(hex2d::Spacing::FlatTop(TILE_SIZE));
+        board.tiles.insert((1, 1), Normal);
+        board
+    }
+    // Godot's ready hook
+    #[export]
+    fn _ready(&self, _owner: godot::Node) {
+        godot_print!("{:#?} tile size, {:?} map", self.tile_size, self.tiles);
+    }
+}
+
+/// The Tiles of the game's Board.
+#[derive(Debug)]
+pub enum Tile {
+    Normal,
+    Road,
+    City,
+    Bridge,
+    BridgeWithRoad,
+    Forest,
+    Cliff,
+    Lake,
+}
+
+#[cfg(test)]
 mod test {
     use crate::{Defense, Strength, Unit};
 
@@ -73,34 +122,34 @@ mod test {
 }
 
 /// The HelloWorld "class"
-#[derive(NativeClass)]
-#[inherit(Node)]
-pub struct HelloWorld;
+///#[derive(NativeClass)]
+///#[inherit(godot::Node)]
+///pub struct HelloWorld;
+///
+/// __One__ `impl` block can have the `#[methods]` attribute, which will generate
+/// code to automatically bind any exported methods to Godot.
+///#[methods]
+///impl HelloWorld {
+///    /// The "constructor" of the class.
+///    fn _init(_owner: godot::Node) -> Self {
+///        HelloWorld
+///    }
+///
+///    // In order to make a method known to Godot, the #[export] attribute has to be used.
+///    // In Godot script-classes do not actually inherit the parent class.
+///    // Instead they are"attached" to the parent object, called the "owner".
+///    // The owner is passed to every single exposed method.
+///    #[export]
+///    fn _ready(&self, _owner: godot::Node) {
+///        // The `godot_print!` macro works like `println!` but prints to the Godot-editor
+///        // output tab as well.
+///        godot_print!("hello, world...");
+///    }
+///}
 
-// __One__ `impl` block can have the `#[methods]` attribute, which will generate
-// code to automatically bind any exported methods to Godot.
-#[methods]
-impl HelloWorld {
-    /// The "constructor" of the class.
-    fn _init(_owner: Node) -> Self {
-        HelloWorld
-    }
-
-    // In order to make a method known to Godot, the #[export] attribute has to be used.
-    // In Godot script-classes do not actually inherit the parent class.
-    // Instead they are"attached" to the parent object, called the "owner".
-    // The owner is passed to every single exposed method.
-    #[export]
-    fn _ready(&self, _owner: Node) {
-        // The `godot_print!` macro works like `println!` but prints to the Godot-editor
-        // output tab as well.
-        godot_print!("hello, world...");
-    }
-}
-
-// Function that registers all exposed classes to Godot
-fn init(handle: gdnative::init::InitHandle) {
-    handle.add_class::<HelloWorld>();
+/// Function that registers all exposed classes to Godot
+fn init(handle: godot::init::InitHandle) {
+    handle.add_class::<Board>();
 }
 
 // macros that create the entry-points of the dynamic library.
