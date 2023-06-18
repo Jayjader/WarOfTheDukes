19db5b8a2a86607721a6973a3882faf9ecea3788
Jayjader <jayjader@Jayjaders-MacBook-Pro.local>
2023-06-18 10:57:29 +0200

    start work on selecting units for movement

--- game_session/unit_layer/unit_layer_root.gd
+++ game_session/unit_layer/unit_layer_root.gd
@@ -5,15 +5,19 @@ const Util = preload("res://util.gd")
 
 const Unit = preload("res://game_session/unit_layer/unit_root.tscn")
 
+
+
 var pieces: Dictionary
 
 func _place_piece(tile: Vector2i, kind: Enums.Unit, faction: Enums.Faction):
-	# todo: this might need to be moved into the tile overlay or game
-	# session scene root script and emit a signal here instead.
-	# also we need a camera for many things including panning w/ acceleration, zooming,
-	# easier syncing of different scene's positions and coordinates
 	var unit = Unit.instantiate()
 	add_child(unit)
 	unit.kind = kind
 	unit.faction = faction
 	unit.position = Util.hex_coords_to_pixel(tile, 60)
+
+signal unit_clicked(kind: Enums.Unit, faction: Enums.Faction, tile: Vector2i)
+
+func make_faction_selectable(faction):
+	for unit in get_children():
+		unit.selectable = unit.faction == faction
