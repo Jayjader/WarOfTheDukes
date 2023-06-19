e4b679f8f23b5f24d8217649a6fa0d046ee4cf1a
Jayjader <njay47@gmail.com>
2023-06-19 16:23:37 +0200

    add map data handler to project autoload

--- board/MapDataHandler.gd
+++ board/MapDataHandler.gd
@@ -1,29 +1,32 @@
 @tool
 extends Node
+class_name MapDataHandler
 
 signal data_saved
-signal data_loaded(map_data: HexMapData)
+signal data_loaded()
 
-func save_data(data):
+@export var map: HexMapData
+
+func save_data():
 	var file = FileAccess.open("./map.data", FileAccess.WRITE)
-	file.store_var(data)
+	file.store_var(map)
 	data_saved.emit()
 
 func load_data():
 	var file = FileAccess.open("./map.data", FileAccess.READ)
 	if file == null:
 		print_debug("map data not found")
-		return null
-	var map = HexMapData.new()
-	var data = file.get_var()
-	if data.get("tiles") != null:
-		map.tiles = data.tiles
-	if data.get("borders") != null:
-		map.borders = data.borders
-	map.zones = data.get("zones", { Orfburg = [], Wulfenburg = [], Kaiserburg = [], BetweenRivers = [], OrfburgTerritory = [], WulfenburgTerritory = [] })
+		map = HexMapData.new()
+	else:
+		var data = file.get_var()
+		if data.get("tiles") != null:
+			map.tiles = data.tiles
+		if data.get("borders") != null:
+			map.borders = data.borders
+		map.zones = data.get("zones", { Orfburg = [], Wulfenburg = [], Kaiserburg = [], BetweenRivers = [], OrfburgTerritory = [], WulfenburgTerritory = [] })
 	print_debug("loaded: %s borders, %s tiles, %s zones" % [
 		len(map.tiles.keys()),
 		len(map.borders.keys()),
 		len(map.zones.Orfburg) + len(map.zones.Wulfenburg) + len(map.zones.Kaiserburg) + len(map.zones.BetweenRivers)
 		])
-	data_loaded.emit(map)
+	data_loaded.emit()
