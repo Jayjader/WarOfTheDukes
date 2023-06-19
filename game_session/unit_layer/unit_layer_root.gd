c6acecee1b16ea42cd336bd980d9fcd37430e732
Jayjader <njay47@gmail.com>
2023-06-19 13:52:09 +0200

    allow selecting units from unit layer

--- game_session/unit_layer/unit_layer_root.gd
+++ game_session/unit_layer/unit_layer_root.gd
@@ -7,17 +7,33 @@ const Unit = preload("res://game_session/unit_layer/unit_root.tscn")
 
 
 
-var pieces: Dictionary
+var starting_pieces: Dictionary = { Vector2i(1, 1): [Enums.Unit.Infantry, Enums.Faction.Orfburg] }
+
+@export var hex_size = 60
 
 func _place_piece(tile: Vector2i, kind: Enums.Unit, faction: Enums.Faction):
 	var unit = Unit.instantiate()
 	add_child(unit)
 	unit.kind = kind
 	unit.faction = faction
-	unit.position = Util.hex_coords_to_pixel(tile, 60)
+	unit.position = Util.hex_coords_to_pixel(tile, hex_size)
+	unit.connect(
+		"selected",
+		func():
+			unit_clicked.emit(
+				unit.kind,
+				unit.faction,
+				Util.nearest_hex_in_axial(unit.position, Vector2i(0, 0), hex_size))
+				)
 
 signal unit_clicked(kind: Enums.Unit, faction: Enums.Faction, tile: Vector2i)
 
 func make_faction_selectable(faction):
 	for unit in get_children():
 		unit.selectable = unit.faction == faction
+
+func _ready():
+	for tile in starting_pieces:
+		var unit = starting_pieces[tile]
+		_place_piece(tile, unit[0], unit[1])
+
--- game_session/unit_layer/unit_root.gd
+++ game_session/unit_layer/unit_root.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 const Enums = preload("res://enums.gd")
 const Drawing = preload("res://drawing.gd")
+const Util = preload("res://util.gd")
 
 @export var kind: Enums.Unit:
 	set(value):
@@ -12,3 +13,27 @@ const Drawing = preload("res://drawing.gd")
 		$Label.add_theme_color_override("font_color", Drawing.faction_colors[value])
 		faction = value
 		# todo: set theme according to faction
+
+@export var selectable: bool = false:
+	set(value):
+		selectable = value
+		if not selectable:
+			_selected = false
+
+var hex_size:
+	get:
+		return get_parent().hex_size
+
+signal selected()
+var _selected: bool = false:
+	set(value):
+		_selected = value
+		selected.emit(_selected)
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+		if selectable:
+			var tile: Vector2 = Util.nearest_hex_in_world(event.position, Vector2i(0, 0), hex_size)[0]
+			if  tile.distance_to(self.position) < hex_size / 2:
+				get_viewport().set_input_as_handled()
+				_selected = not _selected
