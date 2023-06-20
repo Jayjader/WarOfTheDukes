e3ba5f0e700182ff4a6d517e27b98f40f4aaf845
Jayjader <njay47@gmail.com>
2023-06-20 23:18:39 +0200

    minor code reformatting for scene switching

--- main_game_scene.gd
+++ main_game_scene.gd
@@ -4,26 +4,30 @@ const Lobby = preload("res://lobby/lobby_root.tscn")
 const MainMenu = preload("res://main_menu_root.tscn")
 const GameSession = preload("res://game_session/game_session_root.tscn")
 
+func _cleanup_current_mode():
+	get_child(0).queue_free()
+
 var data
 @export var mode: Enums.MainMode:
 	set(value):
 		print_debug("new main mode: %s" % Enums.MainMode.find_key(value))
 		match value:
 			Enums.MainMode.MainMenu:
-				#todo: cleanup existing children
+				_cleanup_current_mode()
 				var main_menu = MainMenu.instantiate()
 				add_child(main_menu)
-				main_menu.connect("new_game_started", _on_main_menu_root_new_game_started)
+				main_menu.new_game_started.connect(_on_main_menu_root_new_game_started)
 			Enums.MainMode.NewGameLobby:
-				$MainMenuRoot.queue_free()
+				_cleanup_current_mode()
 				var lobby = Lobby.instantiate()
 				add_child(lobby)
-				lobby.connect("player_1_faction_confirmed", _on_lobby_player_1_faction_choice_confirmed)
+				lobby.player_1_faction_confirmed.connect(_on_lobby_player_1_faction_choice_confirmed)
 			Enums.MainMode.InGame:
-				$LobbyRoot.queue_free()
+				_cleanup_current_mode()
 				var game_session = GameSession.instantiate()
-				add_child(game_session)
 				game_session.player_1 = data as Enums.Faction
+				add_child(game_session)
+				#game_session.<signal>.connect(...)
 		
 		mode = value
 
