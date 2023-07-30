5747ea29171b69e51f51292923fa04b6b9eb5ecc
jayjader@m1 <njay47@gmail.com>
2023-07-30 09:59:37 +0200

    add support for laptop trackpad scrolling

--- game_session/OverBoardCamera.gd
+++ game_session/OverBoardCamera.gd
@@ -1,14 +1,14 @@
 extends Camera2D
 
-@export var min_zoom = 0.1
+@export var min_zoom: float = 0.375
 
-@export var max_zoom = 2
+@export var max_zoom: float = 1.25
 
-@export var zoom_factor = 0.05
+@export var zoom_factor: float = 0.05
 
-@export var zoom_duration = 0.2
+@export var zoom_duration: float = 0.2
 
-var _zoom_level = 1:
+var _zoom_level: float = 1.0:
 	set(new_zoom):
 		var clamped = clamp(new_zoom, min_zoom, max_zoom)
 		create_tween().tween_property(
@@ -18,6 +18,31 @@ var _zoom_level = 1:
 			zoom_duration
 		).set_ease(Tween.EASE_OUT)
 		_zoom_level = clamped
+
+@export var scroll_panning_threshold: float = 0.1
+
+@export var invert_y_axis: bool = true
+
+func _input(event):
+	# trackpad scrolling support
+	if event is InputEventPanGesture:
+		get_viewport().set_input_as_handled()
+		var y_abs = abs(event.delta.y)
+		# The underlying acceleration driving the OS events we end up receiving seems to run afoul
+		# of the "reactive" tweening we use, locking us into a camera zoom "drift"
+		#var steps = min(3, y_abs / scroll_panning_threshold)
+		#var action_name = "Increase Camera Zoom" if sign(event.delta.y) > 0 else "Decrease Camera Zoom"
+		#for i in range(steps):
+		#	var action = InputEventAction.new()
+		#	action.action = action_name
+		#	action.pressed = true
+		#	Input.parse_input_event(action)
+		if y_abs > scroll_panning_threshold:
+			_zoom_level = log(
+				exp(_zoom_level) * (
+					1 + (int(invert_y_axis)) * (zoom_factor * y_abs * sign(event.delta.y))
+				)
+			)
 
 func _process(_delta):
 	if Input.is_action_pressed("Increase Camera Zoom"):
