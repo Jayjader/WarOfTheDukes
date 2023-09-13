1824e92c385848d282ce3166477436536a4fce72
Jayjader <njay47@gmail.com>
2023-09-13 14:27:40 +0200

    seperate pathing for retreat from movement
    introduce some helper functions
    fix typed array / Array.(filter|reduce) poor interactions
    cleanup dead code

--- board/hex_map_data.gd
+++ board/hex_map_data.gd
@@ -16,50 +16,63 @@ class_name HexMapData
 @export_group("Map Properties")
 @export var hex_size_in_pixels: float = 60
 
-func paths_from(start: Vector2i, max_cost=0) -> Dictionary:
-	var frontier = [[start, 0]]
-	var reached = {start: [start, 0]}
+func neighbors_to(tile: Vector2i):
+	var neighbors = {}
+	for direction in Util.cube_directions:
+		var coords = tile + Vector2i(Util.cube_to_axial(Vector3(direction)))
+		if tiles.has(coords):
+			var border = Util.cube_to_axial(0.5 * Vector3(direction) + Util.axial_to_cube(tile))
+			neighbors[coords] = borders.get(border)
+	return neighbors
 
-	while len(frontier) > 0:
-		frontier.sort_custom(func(a, b): return b[1] > a[1])
-		var next = frontier.pop_front()
-		var next_axial = next[0]
-		var next_accumulated_cost = next[1]
-		var next_cube = Util.axial_to_cube(next_axial)
-		for direction in Util.cube_directions:
-			var to_ = Vector2i(Util.cube_to_axial(next_cube + Vector3(direction)))
-			var to_kind = tiles.get(to_)
-			if to_kind == null:
-				continue
-			var border = Util.cube_to_axial(0.5 * Vector3(direction) + next_cube)
-			var border_kind = borders.get(border)
-			if border_kind == "River" or (border_kind == null and to_kind == "Lake"):
-				continue
-			var movement_cost = next_accumulated_cost + Rules.MovementCost[border_kind if border_kind != null else to_kind]
-			if movement_cost > max_cost:
-				continue
+func is_in_enemy_zoc(tile: Vector2i, enemy_tiles: Array[Vector2i]) -> bool:
+	var tile_in_cube = Util.axial_to_cube(tile)
+	for enemy_tile in enemy_tiles:
+		if borders.get(0.5 * (tile + enemy_tile)) != "River" and Util.cube_distance(tile_in_cube, Util.axial_to_cube(enemy_tile)) <= 1:
+			return true
+	return false
 
-			if to_ in reached:
-				if reached[to_][1] > movement_cost:
-					reached[to_] = [next_axial, movement_cost]
-			else:
-				frontier.push_back([to_, movement_cost])
-				reached[to_] = [next_axial, movement_cost]
+func paths_for_retreat(unit: GamePiece, others: Array[GamePiece]) -> Array[Vector2i]:
+	var valid_tiles: Array[Vector2i] = []
+	var enemy_tiles = others.reduce(func(tiles, other_unit):
+		if other_unit.faction != unit.faction:
+			tiles.append(other_unit.tile)
+		return tiles
+	, [] as Array[Vector2i])
+	var ally_tiles = others.reduce(func(tiles, other_unit):
+		if other_unit.faction == unit.faction:
+			tiles.append(other_unit.tile)
+		return tiles
+	, [] as Array[Vector2i])
+	var neighbor_tiles = neighbors_to(unit.tile)
+	for tile in neighbor_tiles:
+		var border_crossed = neighbor_tiles[tile]
+		if border_crossed == "River":
+			continue
+		elif tile in ally_tiles:
+			continue
+		elif tile in enemy_tiles:
+			continue
+		elif is_in_enemy_zoc(tile, enemy_tiles):
+			continue
+		else:
+			valid_tiles.append(tile)
 
-	return reached
+	return valid_tiles
 
-func is_in_enemy_zoc(unit: GamePiece, enemies) -> bool:
-	return enemies.any(func(e): return e.tile in Util.neighbours_to_tile(unit.tile))
-
-func paths_for(unit: GamePiece, others: Array) -> Dictionary:
+func paths_for(unit: GamePiece, others: Array[GamePiece]) -> Dictionary:
 	var max_cost = unit.movement_points
-	var enemies = others.filter(func(other_unit): return other_unit.faction != unit.faction)
+	var enemy_tiles = others.reduce(func(tiles, other_unit):
+		if other_unit.faction != unit.faction:
+			tiles.append(other_unit.tile)
+		return tiles
+	, [] as Array[Vector2i])
 	var allies = others.filter(func(other_unit): return other_unit.faction == unit.faction)
 	var initial_frontier_datum = {
 		tile = unit.tile,
 		priority = 0,
 		cost_to_reach = 0,
-		is_in_enemy_zoc = is_in_enemy_zoc(unit, enemies),
+		is_in_enemy_zoc = is_in_enemy_zoc(unit.tile, enemy_tiles),
 	}
 	var frontier = PriorityQueue.new();
 	frontier.insert(initial_frontier_datum)
@@ -89,11 +102,11 @@ func paths_for(unit: GamePiece, others: Array) -> Dictionary:
 			if movement_cost > max_cost:
 				continue
 
-			if enemies.any(func(enemy): return enemy.tile == to_):
+			if to_ in enemy_tiles:
 				# moving through enemies is forbidden
 				continue
 
-			var is_moving_into_enemy_zoc = enemies.any(func(e): return to_ in Util.neighbours_to_tile(e.tile))
+			var is_moving_into_enemy_zoc = is_in_enemy_zoc(to_, enemy_tiles)
 			if next.is_in_enemy_zoc and is_moving_into_enemy_zoc:
 			#	# moving through contiguous enemy zone of control tiles is forbidden
 				continue
