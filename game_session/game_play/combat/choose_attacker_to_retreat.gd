2ffd295f66c2e5fe48ef6c1a64ed4edba04f4356
Jayjader <njay47@gmail.com>
2023-09-13 14:30:49 +0200

    enable attacker retreat combat result resolution
    calculate available paths for retreat when user chooses an attacker to retreat
    
    cleanup some of the code

--- game_session/game_play/combat/choose_attacker_to_retreat.gd
+++ game_session/game_play/combat/choose_attacker_to_retreat.gd
@@ -14,7 +14,7 @@ extends CombatSubphase
 @export var retreat_attacker: RetreatAttacker
 @export var choose_ally_to_make_way: ChooseAllyToMakeWay
 
-@onready var unit_layer = Board.get_node("%UnitLayer")
+@onready var unit_layer: UnitLayer = Board.get_node("%UnitLayer")
 
 func _clear():
 	to_retreat.clear()
@@ -23,32 +23,37 @@ func choose_attacker(attacker: GamePiece):
 	assert(attacker in to_retreat)
 	to_retreat.erase(attacker)
 	retreat_attacker.to_retreat = attacker
-	# var has_room = len(allowed_retreat_destinations) > 0
-	var has_room = false
-	# var can_make_way = len(allied_neighbors_on(allowed_retreat_destinations).filter(func(u): return not (u in parent_phase.retreated))) > 0
-	var can_make_way = false
+	var other_live_units: Array[GamePiece] = []
+	for unit in unit_layer.get_children().filter(func(unit): return unit != attacker):
+		other_live_units.append(unit)
+	var allowed_retreat_destinations = MapData.map.paths_for_retreat(attacker, other_live_units)
+	var has_room = len(allowed_retreat_destinations) > 0
 	if has_room:
+		retreat_attacker.can_be_retreated_to = allowed_retreat_destinations
 		phase_state_machine.change_subphase(retreat_attacker)
-	elif can_make_way:
-		choose_ally_to_make_way.previous_subphase = self
-		phase_state_machine.change_subphase(choose_ally_to_make_way)
 	else:
-		attacker.die()
-		parent_phase.died.append(attacker)
-		phase_state_machine.change_subphase(main_combat)
+		# var can_make_way = len(allied_neighbors_on(allowed_retreat_destinations).filter(func(u): return not (u in parent_phase.retreated))) > 0
+		var can_make_way = false
+		if can_make_way:
+			choose_ally_to_make_way.previous_subphase = self
+			phase_state_machine.change_subphase(choose_ally_to_make_way)
+		else:
+			attacker.die()
+			parent_phase.died.append(attacker)
+			phase_state_machine.change_subphase(main_combat)
 
 func _enter_subphase():
 	%SubPhaseInstruction.text = "Choose a unit among the attackers to retreat"
 	assert(len(to_retreat) > 0)
 	for attacker in to_retreat:
 		attacker.unselect()
-	unit_layer.make_faction_selectable(null)
 	unit_layer.unit_selected.connect(__on_unit_selected)
-	for attacker in to_retreat:
-		attacker.selectable = true
+	unit_layer.make_units_selectable(to_retreat)
+	if len(to_retreat) == 1:
+		choose_attacker(to_retreat.front())
 
 func _exit_subphase():
-	unit_layer.make_faction_selectable(null)
+	unit_layer.make_units_selectable([])
 	unit_layer.unit_selected.disconnect(__on_unit_selected)
 
 func __on_unit_selected(unit: GamePiece):
