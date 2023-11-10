85aa5fd5121d75fa0afa29e43868711ce92252f1
Jayjader <njay47@gmail.com>
2023-11-10 12:57:16 +0100

    add basic ai strategy for moving units
    start scaffolding for combat
    
    for #10, #11, #12
    part of #8

---
+++ movement_strategy.gd
@@ -0,0 +1,41 @@
+class_name MovementStrategy
+extends RefCounted
+
+var _random
+
+const BIAIS_WEIGHTS = {
+	"is_minor_objective": 10,
+	"defense_multiplier": 1,
+}
+
+func _init():
+	_random = RandomNumberGenerator.new()
+
+func _is_minor_objective(tile: Vector2i, map: HexMapData) -> bool:
+	return map.zones.Kaiserburg.has(tile) or map.zones.BetweenRivers.has(tile)
+
+func choose_next_mover(moved: Array[GamePiece], allies: Array[GamePiece], enemies: Array[GamePiece], map: HexMapData):
+	var occupied_tiles = allies.map(func(u):return u.tile)
+	occupied_tiles.append_array(enemies.map(func(u):return u.tile))
+	for unit in allies:
+		if unit in moved:
+			continue
+		if _is_minor_objective(unit.tile, map):
+			continue
+		return unit
+	return null
+
+func choose_destination(mover: GamePiece, others: Array[GamePiece], map: HexMapData):
+	var biais = []
+	var occupied_tiles = others.map(func(p): return p.tile)
+	var paths = map.paths_for(mover, others)
+	var choice = mover.tile
+	for tile in paths:
+		if paths[tile].can_stop_here:
+			biais.append([tile,
+			BIAIS_WEIGHTS.is_minor_objective * int(_is_minor_objective(tile, map))
+			+ BIAIS_WEIGHTS.defense_multiplier * Rules.DefenseMultiplier.get(map.tiles[tile], 0)
+			])
+	var by_biais = func(a,b)->bool: return a[1] < b[1]
+	biais.sort_custom(by_biais)
+	return biais.back()[0]
