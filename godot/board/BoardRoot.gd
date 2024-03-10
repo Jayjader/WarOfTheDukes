e92a88fee51bcb7e44f7514273b7589cf4b481b0
Jayjader <njay47@gmail.com>
2024-03-10 17:05:14 +0100

    continue cleanup of old tilemap code

--- godot/board/BoardRoot.gd
+++ godot/board/BoardRoot.gd
@@ -1,34 +1,11 @@
 extends Control
 
-var EditingGroup: StringName = &"map-edit-ui"
-
 @onready var cursor = %PlayerCursor
 func __on_focus_entered():
 	cursor.grab_focus()
 
 func _ready():
-	# resolve @tool and autoload clash
-	#if Engine.is_editor_hint() and get_viewport() is Window:
-	#	get_parent().remove_child(self)
 	MapData.load_data()
-
-@export var report_hovered_hex: bool:
-	get:
-		if self.is_node_ready():
-			return %TileOverlay.report_hovered_hex
-		return false
-	set(value):
-		if self.is_node_ready():
-			%TileOverlay.report_hovered_hex = value
-
-@export var report_clicked_hex: bool:
-	get:
-		if self.is_node_ready():
-			return %TileOverlay.report_clicked_hex
-		return false
-	set(value):
-		if self.is_node_ready():
-			%TileOverlay.report_clicked_hex = value
 
 func paths_from(tile: Vector2i, max_cost: int):
 	return MapData.map.paths_from(tile, max_cost)
--- godot/board/tile_layer/TileOverlay.gd
+++ godot/board/tile_layer/TileOverlay.gd
@@ -1,23 +1,6 @@
-# Helps draw and edit hex tile map data
-# To use, place as child node of map sprite, and anchor as full rect
 extends Node2D
 
-signal hex_hovered(Vector2i)
-signal hex_clicked(tile: Vector2i, kind, zones: Array)
-
 @onready var cursor = %PlayerCursor
-
-@export var report_hovered_hex: bool = false:
-	get:
-		if self.is_node_ready():
-			return cursor.capture_hover
-		return false
-	set(value):
-		if self.is_node_ready():
-			cursor.capture_hover = value
-			if not value:
-				queue_redraw()
-@export var report_clicked_hex: bool = false
 
 func clear_destinations():
 	%MovementRange.destinations.clear()
